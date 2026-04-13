e6b93183d3c08abb8afe39a049061203cb301fdd
Erwann Robin <erwann.robin@blablacar.com>
2026-04-14 01:30:14 +0200

    fix iOS build

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -120,10 +120,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: - Capacitor / URL handling
 
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
-    }
-
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
         return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
     }
