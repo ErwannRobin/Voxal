ea064a59d1dc1c9f01a8903f41ad2bcc97d05616
Erwann Robin <erwann.robin@blablacar.com>
2026-09-15 13:50:37 +0200

    publish new release 0.9.8

--- src/version.js
+++ src/version.js
@@ -1,4 +1,4 @@
 // Voxal version — update VOXAL_VERSION and VOXAL_BUILD_DATE on each release
-const VOXAL_VERSION    = '0.9.7';
-const VOXAL_BUILD_DATE = '2026-08-02';
+const VOXAL_VERSION    = '0.9.8';
+const VOXAL_BUILD_DATE = '2026-09-15';
 const VOXAL_GITHUB_URL = 'https://github.com/ErwannRobin/Voxal';
